402bd13b2c8e31ebae21ef6c83188dd22a238e8b
Samir-Mujkic <samir.mujkic096@gmail.com>
2021-03-15 21:04:05 +0100

    zadatak 3

--- ex03/src/index.js
+++ ex03/src/index.js
@@ -3,14 +3,14 @@ var myGlobalVariable = 10;
 function function1() {}
 notGlobalVariable = 5;
 function function2() {
-    var result ="";
-    if (typeof myGlobalVariable != "undefined"){
-        result += "myglobalVariable: " + myGlobalVariable;
-    }
-    if (typeof notGlobalVariable != "undefined"){
-        result += " notglobalVariable: " + notGlobalVariable;
-    }
-    console.log(result);
+  var result = "";
+  if (typeof myGlobalVariable != "undefined") {
+    result += "myglobalVariable: " + myGlobalVariable;
+  }
+  if (typeof notGlobalVariable != "undefined") {
+    result += " notglobalVariable: " + notGlobalVariable;
+  }
+  console.log(result);
 }
 
 function1();
@@ -19,14 +19,6 @@ function2();
 // Write code above this line
 
 module.exports = {
-    function1,
-    function2
+  function1,
+  function2,
 };
-
-  
-  
-  
-
-
-    
-
