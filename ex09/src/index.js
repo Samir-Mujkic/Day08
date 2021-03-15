af59a8fb8edccb797c4b88caf0b0757a212d94da
Samir-Mujkic <samir.mujkic096@gmail.com>
2021-03-15 16:08:53 +0100

    zadatak 9

---
+++ ex09/src/index.js
@@ -0,0 +1,23 @@
+
+
+//Only change code below this line
+function isLess(a, b){
+//Only change code above this line
+if(a < b){
+    return true;
+   //Only change code below this line 
+} else {
+// Only change code above this line
+    return false
+}
+//Only change code below this line
+}
+console.log(isLess(10, 15));
+console.log(isLess(15, 10));
+//Only change code above this line
+module.exports = isLess;
+
+
+
+
+
