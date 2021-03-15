c06752cfcd913810fb76fbca6a938c4dbefe64a2
Samir-Mujkic <samir.mujkic096@gmail.com>
2021-03-15 21:03:25 +0100

    zadatak 2

--- ex02/src/index.js
+++ ex02/src/index.js
@@ -1,10 +1,9 @@
 // Write code below this line
 function functionPrintInfo(name, age) {
-    console.log(name, age);
-    
-  }
-  functionPrintInfo("John", 23);
-  functionPrintInfo("Michael", 43);
-  
+  console.log(name, age);
+}
+functionPrintInfo("John", 23);
+functionPrintInfo("Michael", 43);
+
 // Write code above this line
 module.exports = functionPrintInfo;
