602d657cf71ea5612e6093d5c7c03065ffdbb194
Samir-Mujkic <samir.mujkic096@gmail.com>
2021-03-15 20:10:43 +0100

    zadatak 14

---
+++ ex14/src/index.js
@@ -0,0 +1,20 @@
+
+
+//Only change code below this line
+function convertStrToInt(str){
+    return parseInt(str);
+    
+   
+}
+
+convertStrToInt("44");
+convertStrToInt("27");
+convertStrToInt("JeffBezos");
+console.log(convertStrToInt("44"));
+
+
+
+
+
+//Only change code above this line
+module.exports = convertStrToInt;
