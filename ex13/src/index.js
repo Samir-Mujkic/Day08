0facc00a851f9adc6832b89599165c62fb59bc25
Samir-Mujkic <samir.mujkic096@gmail.com>
2021-03-15 18:44:14 +0100

    zadatak 13

--- ex13/src/index.js
+++ ex13/src/index.js
@@ -5,5 +5,5 @@ function randomRangeNumber(minNumber, maxNumber){
     return Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber
 }
  console.log(randomRangeNumber(1, 50));
- 
+ //Only change code abode this line
  module.export = randomRangeNumber;
