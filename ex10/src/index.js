bb46944480b6e65e63352a58ec6c79ba30e69c80
Samir-Mujkic <samir.mujkic096@gmail.com>
2021-03-15 22:40:17 +0100

    zadatak 10

--- ex10/src/index.js
+++ ex10/src/index.js
@@ -5,11 +5,11 @@ function mathTest(x, y) {
   }
   return Math.round(Math.pow(Math.sqrt(x) + Math.sqrt(y), 2));
 }
-console.log(mathTest(2,2));
-console.log(mathTest(-2,2));
-console.log(mathTest(2,-2));
-console.log(mathTest(2,8));
-console.log(mathTest(3,3));
-console.log(mathTest(0,0));
+console.log(mathTest(2, 2));
+console.log(mathTest(-2, 2));
+console.log(mathTest(2, -2));
+console.log(mathTest(2, 8));
+console.log(mathTest(3, 3));
+console.log(mathTest(0, 0));
 //Only change code above this line
 module.export = mathTest;
