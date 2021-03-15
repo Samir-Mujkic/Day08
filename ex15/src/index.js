299f5c9b58135c12e0fb81a6b288385e5b6f3c35
Samir-Mujkic <samir.mujkic096@gmail.com>
2021-03-15 20:47:05 +0100

    zadatak 15

---
+++ ex15/src/index.js
@@ -0,0 +1,19 @@
+
+
+//Only change code below this line
+function convertToInt(src){
+  return parseInt(src, 16);
+   
+}
+
+convertToInt("BA");
+convertToInt("F1");
+convertToInt("JeffBezos");
+console.log(convertToInt("BA"));
+
+
+//Only change code above this line
+
+
+
+module.exports = convertToInt;
