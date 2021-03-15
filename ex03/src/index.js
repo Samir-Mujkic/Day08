ee5cb5ac72fed99018d30302e83fbbaf730ba785
Samir-Mujkic <samir.mujkic096@gmail.com>
2021-03-15 13:12:26 +0100

    zadatak3

---
+++ ex03/src/index.js
@@ -0,0 +1,32 @@
+// Write code below this line
+var myGlobalVariable = 10;
+function function1() {}
+notGlobalVariable = 5;
+function function2() {
+    var result ="";
+    if (typeof myGlobalVariable != "undefined"){
+        result += "myglobalVariable: " + myGlobalVariable;
+    }
+    if (typeof notGlobalVariable != "undefined"){
+        result += " notglobalVariable: " + notGlobalVariable;
+    }
+    console.log(result);
+}
+
+function1();
+function2();
+
+// Write code above this line
+
+module.exports = {
+    function1,
+    function2
+};
+
+  
+  
+  
+
+
+    
+
