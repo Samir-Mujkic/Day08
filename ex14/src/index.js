618fdbf18e7f9b47557ffee0a55214b20071a176
Samir-Mujkic <samir.mujkic096@gmail.com>
2021-03-15 21:01:48 +0100

    zadatak 14

--- ex14/src/index.js
+++ ex14/src/index.js
@@ -1,21 +1,13 @@
-const convertToInt = require("../../ex15/src");
 
 
 //Only change code below this line
-function convertStrToInt(str){
-    return parseInt(str);
-    
-   
+function convertStrToInt(str) {
+  return parseInt(str);
 }
 
 convertStrToInt("44");
 convertStrToInt("27");
 convertStrToInt("JeffBezos");
-
-
-console.log(convertToInt("BA"));
-
-
 
 //Only change code above this line
 module.exports = convertStrToInt;
