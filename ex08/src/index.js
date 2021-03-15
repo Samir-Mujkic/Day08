a8775812a1b21c70e630c636a125b5b08262d569
Samir-Mujkic <samir.mujkic096@gmail.com>
2021-03-15 21:08:50 +0100

    zadatak 8

--- ex08/src/index.js
+++ ex08/src/index.js
@@ -1,7 +1,7 @@
 //Only change code below this line
 var total = 0;
-function funciWithArg(digit){
-    total = (digit + 9) / 5;
+function funciWithArg(digit) {
+  total = (digit + 9) / 5;
 }
 funciWithArg(6);
 //Only change code above this line
