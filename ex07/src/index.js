411d24ad33eedcb2d7a00df094b53a3159952766
Samir-Mujkic <samir.mujkic096@gmail.com>
2021-03-20 12:44:28 +0100

    zadatak7

--- ex07/src/index.js
+++ ex07/src/index.js
@@ -1,12 +1,12 @@
 // Only change code below this line
 var sum = 0;
 function addThree() {
-  sum = sum + 3;
+   sum += + 3;
   console.log(sum);
 }
 
 function addFive() {
-  sum = sum + 5;
+  sum += 5;
   console.log(sum);
 }
 addThree();
